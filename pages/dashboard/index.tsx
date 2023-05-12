95fdcc01aa6b2e8f54c021d449210a9983e5e00a
xmrrabbitx <hadi.mirzaie300@gmail.com>
2023-05-12 20:00:41 +0330

    fix jwt issue

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,31 +1,19 @@
 import { useState, useEffect } from 'react';
-import { useSession, getSession } from 'next-auth/react';
 import { useRouter } from "next/router"
+import Cookies from 'js-cookie';
+import jwt from 'jsonwebtoken';
 
 interface Data {
     [key: string]: string;
 }
 
-export default function dashboard(){
-
-    const { data: session,status } = useSession()
-    const loading = status === 'loading'
-
-    const { data }:any = useSession();
-    
+export default function dashboard(props:any){
 
     const router = useRouter();
+
+    const token = Cookies.get("authToken")
    
     const [res, setRecentItem] = useState<Data>({});
-
-    if (!session && !loading) {
-      
-        router.push('/auth/signin')
-     
-     
-    }else{
-
-        
 
     const createSol = async (data:any)=>{
 
@@ -37,18 +25,39 @@ export default function dashboard(){
                 setRecentItem(result);
         
             } catch (error) {
-               console.log('Error fetching data:', error);
+
+                console.log('Error fetching data:', error);
+
              }
-          
           
     }
     return(
         <>
-                <>dashboard page</>
-                <p> Hello {data.session.user.name} , welcome to crypto dorea </p>
-                <button className="bg-blue-500" onClick={(e) => createSol(data)}>create</button>
+        
+            <>dashboard page</>
+            <p> Hello  , welcome to crypto dorea </p>
+            <button className="bg-blue-500" onClick={(e) => createSol("hadi")}>create</button>
+
+
+            <div className='container max-w-full' >
+                <div className='grid grid-cols-2 gap-1 mt-10'> 
+
+                </div>
+            </div>
+
+
         </>
     )
 
-    }
+    
 }
+
+export async function getServerSideProps() {
+  
+    const secretKey:any = process.env.JwtSecretKey
+  
+  return {
+    props:{secretKey}
+  }
+  
+  }
