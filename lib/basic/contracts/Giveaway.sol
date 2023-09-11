c1de6a9ab681122cbffef59dd652094c1880a5cb
xmrrabbitx <hadi.mirzaie300@gmail.com>
2023-09-12 03:24:03 +0330

    frameworking contract

--- lib/basic/contracts/Giveaway.sol
+++ lib/basic/contracts/Giveaway.sol
@@ -3,16 +3,55 @@ pragma solidity >= 0.4.0 < 0.9.0;
 
 contract Giveaway {
 
-      uint votes;
+    struct UserData {
+        string name;
+        string email;
+        address userAddress;
+        uint256 giveawayCode;
+    }
 
-      constructor(){
+    address private owner;
+    UserData[] public usersData;
 
-        votes = 1234;
+    constructor(){
+
+        
+    }
+
+    function checkUser(string memory _name, string memory _email, address _userAddress, uint256  _giveawayCode) public view returns (bool){
+
+      for(uint256 i =0; i < usersData.length; i++){
+
+        UserData storage user = usersData[i];
+
+        if (keccak256(bytes(user.name)) == keccak256(bytes(_name)) &&
+            keccak256(bytes(user.email)) == keccak256(bytes(_email)) &&
+            user.userAddress == _userAddress &&
+            user.giveawayCode == _giveawayCode ){
+          return true;
+        }
       }
 
-      function show() public view returns(uint){
+      return false;
+    } 
+
+    function addWinner(string memory _name, string memory _email,address _userAddress, uint256  _giveawayCode) public {
+
+      UserData memory winnerUser = UserData({
+        name: _name,
+        email: _email,
+        userAddress: _userAddress,
+        giveawayCode: _giveawayCode
+      });
+
+      usersData.push(winnerUser);
       
-        return votes;
-      }
+    }
 
+
+    function showWinner() public view returns(UserData[] memory){
+
+      return usersData;
+
+    }
 }
