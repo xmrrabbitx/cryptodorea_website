8a119f2e50b71bc8f2af40a3c0f812eeef545d4a
xmrrabbitx <hadi.mirzaie300@gmail.com>
2023-09-24 06:53:07 +0330

    add footer

--- pages/components/landing/Footer.tsx
+++ pages/components/landing/Footer.tsx
@@ -1,8 +1,39 @@
+import Image from 'next/image'
+import styles from '@/styles/landing/Home.module.css'
+import LinkedinLogo from "@/public/pics/linkedin.svg"
+import TwitterLogo from "@/public/pics/twitter.svg"
 
 export default function Footer(){
     return(
-        <footer>
-            Footer Section
+        <footer className={`${styles.footer} grid grid-cols-1 mt-36 pl-24 pt-16`}>
+          <div className="flex flex-grid">
+            <div className='w-12/12'>
+              <p className='text-xl'>About Us</p>
+              <a href='#ourgoal' className='grid grid-cols-1 text-1xl font-bold pt-2'>
+                  Our Goal
+              </a>
+            </div>
+            <div className='w-12/12 pl-16'>
+              <p  className='text-xl'>Community</p>
+              <div className='flex flex-grid items-center justify-center gap-2 pt-2'>
+              
+              <a href="https://linkedin.com/cryptodorea" >
+                <Image className="" width={30} height={30} src={TwitterLogo} alt="Twitter logo" />
+              </a>
+              <a href="https://x.com/cryptodorea" >
+                <Image className='' width={30} height={30} src={LinkedinLogo} alt="Linkedin logo" />
+              </a>
+              
+              
+              </div>
+            </div>
+           
+          </div>
+
+          <p className='text-center font-bold text-gray-800 mt-40 mb-5'> 
+              @cryptodorea
+          </p>
+
         </footer>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,31 +5,38 @@ import styles from '@/styles/landing/Home.module.css'
 import titleMainPic from "@/public/pics/titleCryptoPic.svg"
 import BitcoinPic from "@/public/pics/Bitcoin.svg"
 import EthPic from "@/public/pics/Eth.svg"
+import productsample1 from "@/public/pics/productSample_1.svg"
 import Cookies from 'js-cookie';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef  } from 'react';
+
 
 
 const images = [
-  'productSample1.svg',
-  'productSample2.svg',
-  'productSample3.svg',
-  'productSample4.svg',
+  'productSample_1.svg',
+  'productSample_2.svg',
+  'productSample_3.svg',
+  'productSample_4.svg',
 ];
 
 export default function Home(props:any) {
 
+
   const [currentImage, setCurrentImage] = useState(0);
+
 
   useEffect(():any => {
     const interval = setInterval(() => {
-      if(currentImage > 3){
-        setCurrentImage(0);
+      if(currentImage >= 3){
+        
+        setCurrentImage(-1);
       }
-      setCurrentImage((prevImage) => (prevImage + 1));
+        setCurrentImage((prevImage) => (prevImage + 1));
+      console.log(currentImage)
     }, 3000);
 
     return () => clearInterval(interval);
   });
+
 
   return (
     <div>
@@ -51,7 +58,7 @@ export default function Home(props:any) {
                 Crypto Giveaways <br />
                 to Loyal Customers
               </p>
-              <p className="text-1xl mt-4">
+              <p className="text-1xl mt-6">
                 Create Crypto Loyalty Program for your Business
               </p>
               <p className='flex mt-4'>
@@ -60,9 +67,10 @@ export default function Home(props:any) {
                 <span><Image className='ml-2' src={EthPic} alt='no Ethereum image' width={35} height={35} /></span>
               </p>
               <div>
-                  <button id={styles.joinButtonTitle} className="w-12/12 pt-2 pb-2 text-center mt-3">
+                  <button id={styles.joinButtonAnchore} className="w-12/12 pt-2 pb-2 text-center mt-5">
                       Join Waitlist
                   </button>
+                  <input type='text' placeholder='your email address...' className='text-md ml-3 pl-8 p-2 border border-2 border-solid border-gray-300 rounded-md'  />
               </div>
               <div>
                   <div></div>
@@ -75,7 +83,7 @@ export default function Home(props:any) {
             </div>
           </div>
 
-          <div className='grid grid-cols-5 gap-16 mt-20'>
+          <div className='grid grid-cols-5 gap-16 mt-28'>
             <div className='w-12/12'></div>
             <div className='w-12/12'>
               <div className={`${styles.percentsNumbers} text-center text-6xl`}>21%</div>
@@ -112,6 +120,10 @@ export default function Home(props:any) {
             <div className='w-12/12'></div>
           </div>
 
+          <div className='flex items-center justify-center mt-8'>
+              <hr className='h-0.5 w-8/12 ml-24 bg-gray-200 border-0 bg-black mt-24' />
+          </div>
+
           <div className='grid grid-cols-2 gap-1 mt-28'> 
             <div className='w-12/12 pl-28 mt-24'>
               <p id={styles.doreaDefinitionTitle} className='text-xl font-bold'>What is Crypto Dorea?</p>
@@ -120,12 +132,12 @@ export default function Home(props:any) {
               </div>
             </div>
             <div className='w-12/12'>
-              <div className="image-slider">
-                {currentImage}
+              <div className="image-slider pl-20"> 
+                
                   {images.map((image, index) => (
                     <Image
                       key={index}
-                      src={`${src}${image}`}
+                      src={`/pics/${image}`}
                       alt={`Image ${index + 1}`}
                       className={`${index === currentImage ? styles.slideVisible : styles.slideHidden} ${styles.slideshow} `}
                       width={100}
@@ -133,9 +145,69 @@ export default function Home(props:any) {
                       quality={100}
                     />
                   ))}
+
                 </div>
               </div>
           </div>
+
+          <div className='grid grid-cols-1 gap-1 mt-32'>
+            <div className='w-12/12 pl-28'>
+              <p id={styles.doreaDefinitionTitle} className='text-xl font-bold'>Why Crypto Dorea?</p>
+                <div className='grid grid-cols-5 gap-5 mt-6'>
+                  <div className={` 'w-12/12'`}></div>
+                  <div id='infoBoxs' className={`${styles.infoBoxs} w-12/12 text-lg font-bold p-8`}>
+              
+                  Integrated into <br /> your Website
+                  </div>
+                  <div id='infoBoxs' className={`${styles.infoBoxs} w-12/12 text-lg font-bold p-8`}>Simple Auto Pay Mechanism</div>
+                  <div id='infoBoxs' className={`${styles.infoBoxs}  w-12/12 text-lg font-bold p-8`}>Low Cost <br/> Blockchain Fees</div>
+                  <div className={`'w-12/12'`}></div>
+                </div>
+            </div>
+          </div>
+
+          <div className='grid grid-cols-1 gap-1 mt-16'>
+            <div className='w-12/12 pl-28 mt-24'>
+            <p id={styles.doreaDefinitionTitle} className='text-xl font-bold'>How it works?</p>
+            <div className='grid grid-cols-5 gap-5'>
+                  <div className={` 'w-12/12'`}></div>
+                  <div id='infoBoxs' className={`${styles.infoBoxs} w-12/12 text-lg font-bold p-8`}>
+                    Create Crypto Giveaway Campaign
+                  </div>
+                  <div id='infoBoxs' className={`${styles.infoBoxs} w-12/12 text-lg font-bold p-8`}>Customize <br/> Giveaway Campaign</div>
+                  <div id='infoBoxs' className={`${styles.infoBoxs}  w-12/12 text-lg font-bold p-8`}>Auto Pay <br/> to your Customers</div>
+                  <div className={`'w-12/12'`}></div>
+                </div>
+            </div>
+          </div>
+
+
+          <div className='grid grid-cols-1 gap-1 mt-28'>
+            <div className='w-12/12 flex items-center justify-center'>
+                  <input type='text' placeholder='your email address...' className='text-md w-96 pl-8 p-3 border-2 border-solid border-gray-300 rounded-md'  />
+            </div>
+            <div className='w-12/12 flex items-center justify-center'>
+                  <button id={styles.joinButton} className="w-12/12 pt-2 pb-2 text-center mt-3">
+                      Join Waitlist
+                  </button>
+            </div>
+          </div>
+
+          <div className='flex items-center justify-center mt-4'>
+              <hr className='h-0.5 w-8/12 ml-24 bg-gray-200 border-0 bg-black mt-24' />
+          </div>
+
+          <div className='grid grid-cols-1 gap-1 mt-20'> 
+            <div className='w-8/12 pl-28'>
+              <p id={styles.doreaDefinitionTitle} className='text-xl font-bold'>Our Goal?</p>
+              <div id={styles.doreaDefinition} className='mt-5 ml-14 pr-3 pl-7 pt-7 pb-7 rounded-md'>
+                <p id={styles.doreaDefinitionText} className='text-lg font-bold leading-10'>
+                We are a team of specialists who believe everyone deserves more. So we gather together as the Dorea team to provide the best suit for your business. We aim for small- to mid-sized businesses because most crypto loyalty programs are expensive and not efficient. There is no limitation in crypto dorea; you can launch your own crypto campaign and give rewards to your most loyal customers using our Auto-Pay system at your specific date and time. We believe in privacy, so none of the information of your users will be saved in our database. All the processes are decentralized and transparent on supported blockchains.
+                </p>
+              </div>
+            </div>
+          </div>
+
 
         </div>
 
