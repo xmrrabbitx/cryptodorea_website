4acdabbc9bc060f0c36c8b8fb9d548fe95874537
ahmad <bersewrk@gmail.com>
2024-04-02 17:17:30 +0330

    add smart contract compile api

---
+++ pages/api/smartContract/compile.ts
@@ -0,0 +1,30 @@
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {isEmpty} from "lodash";
+var solc = require('solc');
+
+
+export default function compile(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+    const smartContract = req.body;
+
+    if(!isEmpty(smartContract)) {
+
+        var output = JSON.parse(solc.compile(JSON.stringify(smartContract)));
+
+        let contractInfo:any = [];
+        for (let contractName in output.contracts['test.sol']) {
+
+            contractInfo['abi'] = JSON.stringify(output.contracts['test.sol'][contractName].abi);
+            contractInfo['bytecode'] = output.contracts['test.sol'][contractName].evm.bytecode.object;
+
+        }
+
+        return res.status(200).json({success: {"abi":contractInfo['abi'],"bytecode":contractInfo['bytecode']}})
+
+    }
+
+}
